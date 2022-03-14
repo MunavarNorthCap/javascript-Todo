dc9180d3b8eb50bffb10dd9c00d442a0e2c74c37
MunavarNorthCap <msheik@northcapconsulting.com>
2022-03-14 18:45:39 +0530

    Todo by using Mustache.JS

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-var todos = [];
-
-// let form = document.getElementById("form");
-// form.addEventListener('submit', function(e){
-//     e.preventDefault();
-// })
+let jsonList = [];
 
 var count = 0;
 function increment() {
@@ -11,114 +6,91 @@ function increment() {
   document.getElementById("incrementVal").innerText = count;
 }
 function decrement() {
-  count = count -1;
+  count = count - 1;
   document.getElementById("incrementVal").innerText = count;
 }
 
-function update(values) {
-    document.getElementById("todoList").innerHTML = ""
-  for (let todo of values) {
-    document.getElementById("todoList").innerHTML += `<div class="todo"
-    style="text-decoration: ${todo.done ? "line-through" : "auto"};"> 
-    <input type="radio" class="custom-radio" onclick="done(${todo.id})" /> 
-    ${todo.id}  - ${todo.text} - ${todo.timestamp}
-    <button class='trash-btn' onclick="delJSON(${todo.id})">
-    <i class="fa-solid fa-x"></i></button>
-    </div>`
-  }
-  // console.log(todos);
+function addTodo(values) {
+  // console.log(values);
+  document.getElementById("todoList").innerHTML = "";
+  // for (let todo of values) {
+  //     document.getElementById("todoList").innerHTML += `<div class="todo"
+  //         style="text-decoration: ${todo.done ? "line-through" : "auto"};">
+  //         <input type="radio" class='custom-radio' onclick="done(${todo.id})" />
+  //         ${todo.id}  - ${todo.text} - ${todo.timestamp}
+  //         <button class='trash-btn' onclick="delJSON(${todo.id})">
+  //         <i class="fa-solid fa-x"></i></button>
+  //     </div>` }    // console.log(jsonList);
+
+  // var todoInput = document.getElementById('todoInput').value;
+  var template = document.getElementById("template").innerHTML;
+  // var todoList = document.getElementById("todoList").innerHTML;
+  var render = Mustache.render(template, { list: values });
+  document.getElementById("todoList").innerHTML = render;
 }
+
 let id = 0;
 function add() {
-	id++;
-	todos.push({
-  	id: id,
+  id++;
+  jsonList.push({
+    id: id,
     text: document.getElementById("todoInput").value,
     timestamp: new Date().toUTCString(),
-    // color: id % 2 === 0 ?  "green" : "red",
     done: false,
-  })
-  document.getElementById("todoInput").value = ""
-  update(todos);
+  });
+  document.getElementById("todoInput").value = "";
+  addTodo(jsonList);
   increment();
 }
 function done(id) {
-	// todos = todos.map((td) => {
-  // 	if (td.id === id) {
-  //   	return {...td, done: !td.done}
-  //   }
-  //   return td;
-  // })
-  // console.log(todos);
-  
-  for (var i = 0; i < todos.length; i++) {
-    var td = todos[i];  // console td = {id: 1, text: '', timestamp: 'Sat, 05 Mar 2022 09:55:03 GMT', done: false}
-    // todos = new Array(td);
-    
+  for (let i = 0; i < jsonList.length; i++) {
+    var td = jsonList[i];
     if (td.id === id) {
       td.done = !td.done;
     }
-    console.log(td);
+    // console.log(td);
   }
-  update(todos);
+  addTodo(jsonList);
 }
-function delJSON(id){
-  todos = todos.filter((td) => {
-    return td.id !== id;
-  })
-  if(count> 0){
-    decrement();
+function delJSON(id) {
+  for (let i = 0; i < jsonList.length; i++) {
+    if (jsonList[i].id === id) {
+      jsonList.splice(i, 1);
+    }
   }
-  update(todos);
-
-  // for(let i=0; i<jsonList.length; i++) {
-  //   if(jsonList[i].id === id) {
-  //     jsonList.splice(i,1)
-  //   }
-  // }
-  // addTodo(jsonList);
-  // decrement();
+  addTodo(jsonList);
+  if (count > 0) decrement();
 }
 function showAll() {
-	update(todos);
+  addTodo(jsonList);
 }
 function active() {
-  update(todos.filter((t) => !t.done));
-
-  // let activeArr = [];
-  // for(let todo of jsonList){
-  //   if(!todo.done) {
-  //     activeArr.push(todo);
-  //   }
-  // }
-  // addTodo(activeArr);
+  let activeArr = [];
+  for (let todo of jsonList) {
+    if (!todo.done) {
+      activeArr.push(todo);
+    }
+  }
+  addTodo(activeArr);
 }
 function completed() {
-	update(todos.filter((t) => t.done));
-
-  // let completedArr = [];
-  // for(let todo of jsonList) {
-  //   if(todo.done){
-  //     completedArr.push(todo);
-  //   }
-  // }
-  // addTodo(completedArr);
+  let completedArr = [];
+  for (let todo of jsonList) {
+    if (todo.done) {
+      completedArr.push(todo);
+    }
+  }
+  addTodo(completedArr);
 }
 function clearCompleted() {
-  if(todos.length>0 && todos.some(elm => elm.done === true)) {
-    todos = todos.filter((t) => !t.done)
-    update(todos);
-    decrement();
+  let clearArr = [];
+  for (let todo of jsonList) {
+    if (!todo.done) {
+      clearArr.push(todo);
+    }
   }
+  jsonList = clearArr;
+  addTodo(jsonList);
 
-  // let clearArr = [];
-  // for(let todo of jsonList) {
-  //   if(!todo.done) {
-  //     clearArr.push(todo)      // delete clearArr.push(todo)
-  //   }
-  // }
-  // jsonList = clearArr;
-  // addTodo(jsonList);
-  
-  // if(count > 0) decrement();
+  if (count > 0) decrement();
 }
